e5db2b9774e29192dcb99648f1d637645c22ab41
LittleGiantBaek <horowitz@hanmail.net>
2024-10-10 09:20:51 +0900

    Create MinHeap.js

---
+++ heap/MinHeap.js
@@ -0,0 +1,71 @@
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  getParentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  getLeftChildIndex(index) {
+    return index * 2 + 1;
+  }
+
+  getRightChildIndex(index) {
+    return index * 2 + 2;
+  }
+
+  swap(index1, index2) {
+    const temp = this.heap[index1];
+    this.heap[index1] = this.heap[index2];
+    this.heap[index2] = temp;
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (index > 0 && this.heap[index] < this.heap[this.getParentIndex(index)]) {
+      this.swap(index, this.getParentIndex(index));
+      index = this.getParentIndex(index);
+    }
+  }
+
+  extractMin() {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+    
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+    return min;
+  }
+
+  heapifyDown() {
+    let index = 0;
+    while (this.getLeftChildIndex(index) < this.heap.length) {
+      let smallerChildIndex = this.getLeftChildIndex(index);
+      if (
+        this.getRightChildIndex(index) < this.heap.length &&
+        this.heap[this.getRightChildIndex(index)] < this.heap[smallerChildIndex]
+      ) {
+        smallerChildIndex = this.getRightChildIndex(index);
+      }
+      if (this.heap[index] < this.heap[smallerChildIndex]) break;
+      this.swap(index, smallerChildIndex);
+      index = smallerChildIndex;
+    }
+  }
+}
+
+// 사용 예시
+const minHeap = new MinHeap();
+minHeap.insert(10);
+minHeap.insert(5);
+minHeap.insert(3);
+console.log(minHeap.extractMin()); // 3
+console.log(minHeap.extractMin()); // 5
+console.log(minHeap.extractMin()); // 10
